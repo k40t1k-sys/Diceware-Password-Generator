c04c736b173d4c27cb3b83f85dc90f83d87226d1
Abhinav <32412878+k40t1k-sys@users.noreply.github.com>
2020-12-10 00:36:37 +0530

    Update diceware.py

--- diceware.py
+++ diceware.py
@@ -1,5 +1,8 @@
 import time
 
+# Implementation of random funtion.
+# Not using pre built library for randomization for dice roll.
+
 def random():
     x = time.thread_time_ns()
     a = 8974556217
@@ -8,25 +11,37 @@ def random():
     x = (a*x + c) % m
     return x
 
+
+# Funtion to get the number after 5 dice rolls 
+# to find the corresponding letter in the dictionary. 
+
 def dicenumber():
     diceout = ""
     for i in range(5): # 5 dice rolls
         diceout += str((random() % 6) + 1) # add result of dice roll to variable
     return diceout
 
+
+# Get the word corrresponding to the random number genrated from the 5 dice rolls.
+
 def diceware(x):
     wordlist = open('dictionary.txt', 'r')
     for line in wordlist:
         if line.startswith(x):
+            wordlist.close()
             return line.lstrip('123456\t')
+
+# Ask the user for no. words to be present in the password.
+# The more no of words present the more hard it is to break the code.
 
 limit = int(input("How maany words do you want in your password : "))
 
 pswd = ''
 
 for i in range(limit): 
-    pswd += diceware(dicenumber()).strip('\n') + '-' # increase number for longer passphrases
+    pswd += diceware(dicenumber()).strip('\n') + '-'
 
-
+# Displaying the password using charachter seprated format.
+# The charachter can be anything including a white space(' ') or a simple dash('-').
 
 print(pswd.strip('-'))
